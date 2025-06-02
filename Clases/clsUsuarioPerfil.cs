70bbca58d782cf29b2c6e5f25893cda9298182ec
FelipeRamirezRios <c4ab7cc06@gmail.com>
2025-06-02 09:46:21 -0500

    Add files via upload

---
+++ Clases/clsUsuarioPerfil.cs
@@ -0,0 +1,82 @@
+﻿using Plantilla.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Plantilla.Clases
+{
+    public class clsUsuarioPerfil
+    {
+        private VentaDeCarrosTallerEntities ITM_Ventas = new VentaDeCarrosTallerEntities(); // Objeto de la base de datos permite manipular el CRUD de los objetos generados
+
+        public Usuario_Perfil usuariop { get; set; }// permite manipular o acceder a los atributos de la tabla Usuario perfil
+
+        public string Insertar()
+        {
+            try
+            {
+                ITM_Ventas.Usuario_Perfil.Add(usuariop);
+                ITM_Ventas.SaveChanges();
+                return "Perfil de Usuario Registrado";
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar Perfil de Usuario: " + ex.Message;
+            }
+        }
+
+
+        public string Eliminar()
+        {
+            try
+            {
+                Usuario_Perfil usu = Consultar(usuariop.id);
+                if (usu == null)
+                {
+                    return "el Perfil de Usuario no existe";
+                }
+                ITM_Ventas.Usuario_Perfil.Remove(usuariop);
+                ITM_Ventas.SaveChanges();
+                return "Se eliminó el Perfil de Usuario exitosamente";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+                Usuario_Perfil usa = Consultar(usuariop.id);
+                if (usa == null)
+                {
+                    return "El Perfil de Usuario no es valido";
+                }
+                ITM_Ventas.Usuario_Perfil.AddOrUpdate(usuariop);
+                ITM_Ventas.SaveChanges();
+                return "Se actualizo correctamente";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public Usuario_Perfil Consultar(int User)
+        {
+
+                Usuario_Perfil u = ITM_Ventas.Usuario_Perfil.FirstOrDefault(v => v.id == User);
+                return u;
+
+        }
+
+        public List<Usuario_Perfil> ConsultarTodos()
+        {
+            return ITM_Ventas.Usuario_Perfil.ToList();
+        }
+    }
+}
---
+++ Clases/clsVehiculoenTallerUbicacion.cs
@@ -0,0 +1,80 @@
+﻿using Plantilla.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Plantilla.Clases
+{
+    public class clsVehiculoenTallerUbicacion
+    {
+        private VentaDeCarrosTallerEntities ITM_Ventas = new VentaDeCarrosTallerEntities(); // Objeto de la base de datos permite manipular el CRUD de los objetos generados
+
+        public Vehiculo_en_Taller_Ubicacion vehiculoEnTallerUbicacion { get; set; }// permite manipular o acceder a los atributos de la tabla Venta de carros
+
+        public string Insertar()
+        {
+            try
+            {
+                ITM_Ventas.Vehiculo_en_Taller_Ubicacion.Add(vehiculoEnTallerUbicacion);
+                ITM_Ventas.SaveChanges();
+                return "Vehiculo en Taller Registrado";
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar vehiculo: " + ex.Message;
+            }
+        }
+
+
+        public string Eliminar()
+        {
+            try
+            {
+                Vehiculo_en_Taller_Ubicacion vehiTaller = Consultar(vehiculoEnTallerUbicacion.ID_Vehiculo_Ubicacion);
+                if (vehiTaller == null)
+                {
+                    return "el vehiculo en taller no existe";
+                }
+                ITM_Ventas.Vehiculo_en_Taller_Ubicacion.Remove(vehiculoEnTallerUbicacion);
+                ITM_Ventas.SaveChanges();
+                return "Se eliminó el vehiculo en taller exitosamente";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+                Vehiculo_en_Taller_Ubicacion veh = Consultar(vehiculoEnTallerUbicacion.ID_Vehiculo_Ubicacion);
+                if (veh == null)
+                {
+                    return "El vehiculo no es valido";
+                }
+                ITM_Ventas.Vehiculo_en_Taller_Ubicacion.AddOrUpdate(vehiculoEnTallerUbicacion);
+                ITM_Ventas.SaveChanges();
+                return "Se actualizo correctamente";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public Vehiculo_en_Taller_Ubicacion Consultar(int VehiculoTaller)
+        {
+            Vehiculo_en_Taller_Ubicacion veh1 = ITM_Ventas.Vehiculo_en_Taller_Ubicacion.FirstOrDefault(v => v.ID_Vehiculo_Ubicacion == VehiculoTaller);
+            return veh1;
+        }
+
+        public List<Vehiculo_en_Taller_Ubicacion> ConsultarTodos()
+        {
+            return ITM_Ventas.Vehiculo_en_Taller_Ubicacion.ToList();
+        }
+    }
+}
